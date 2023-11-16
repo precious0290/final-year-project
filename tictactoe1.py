fead694f2ab3524c80d6a37192632061c02be006
Precious <precious.deremo.2021@mumail.ie>
2023-11-16 20:53:01 +0000

    added the simulations part in the main. Need to fix some few issues tho

--- tictactoe1.py
+++ tictactoe1.py
@@ -2,6 +2,7 @@
 
 countSameChar = 0
 list_of_positions = [[0,0]]
+win = ''
 #rowList = []
 #colList = []
 class Tictactoe:
@@ -40,65 +41,82 @@ class Tictactoe:
 
 
     def check(self):
+        global win
         # Horizontal wins X
         if((self.gameboard[0][0] == 'x' and self.gameboard[0][1] == 'x' and self.gameboard[0][2]== 'x')):
-            print("\nx wins!") 
+            print("\nx wins!")
+            win = 'x'
             return True
         if((self.gameboard[1][0] == 'x' and self.gameboard[1][1] == 'x' and self.gameboard[1][2]== 'x')):
-            print("\nx wins!") 
+            print("\nx wins!")
+            win = 'x' 
             return True
         if((self.gameboard[2][0] == 'x' and self.gameboard[2][1] == 'x' and self.gameboard[2][2]== 'x')):
             print("\nx wins!")
+            win = 'x'
             return True
             
         # Vertical Wins X
         if((self.gameboard[0][0] == 'x' and self.gameboard[1][0] == 'x' and self.gameboard[2][0]== 'x')):
-            print("\nx wins!") 
+            print("\nx wins!")
+            win = 'x'
             return True
         if((self.gameboard[0][1] == 'x' and self.gameboard[1][1] == 'x' and self.gameboard[2][1]== 'x')):
-            print("\nx wins!") 
+            print("\nx wins!")
+            win = 'x' 
             return True
         if((self.gameboard[0][2] == 'x' and self.gameboard[1][2] == 'x' and self.gameboard[2][2]== 'x')):
             print("\nx wins!")
+            win = 'x'
             return True
         
         # Diagonal wins X
         if((self.gameboard[0][0] == 'x' and self.gameboard[1][1] == 'x' and self.gameboard[2][2]== 'x')):
-            print("\nx wins!") 
+            print("\nx wins!")
+            win = 'x' 
             return True
         if((self.gameboard[0][2] == 'x' and self.gameboard[1][1] == 'x' and self.gameboard[2][0]== 'x')):
-            print("\nx wins!") 
+            print("\nx wins!")
+            win = 'x' 
             return True
         
         # Horizontal wins O
         if((self.gameboard[0][0] == 'o' and self.gameboard[0][1] == 'o' and self.gameboard[0][2]== 'o')):
-            print("\no wins!") 
+            print("\no wins!")
+            win = 'o' 
             return True
         if((self.gameboard[1][0] == 'o' and self.gameboard[1][1] == 'o' and self.gameboard[1][2]== 'o')):
-            print("\no wins!") 
+            print("\no wins!")
+            win = 'o' 
             return True
         if((self.gameboard[2][0] == 'o' and self.gameboard[2][1] == 'o' and self.gameboard[2][2]== 'o')):
             print("\no wins!")
+            win = 'o'
             return True
             
         # Vertical Wins O
         if((self.gameboard[0][0] == 'o' and self.gameboard[1][0] == 'o' and self.gameboard[2][0]== 'o')):
-            print("\no wins!") 
+            print("\no wins!")
+            win = 'o' 
             return True
         if((self.gameboard[0][1] == 'o' and self.gameboard[1][1] == 'o' and self.gameboard[2][1]== 'o')):
-            print("\no wins!") 
+            print("\no wins!")
+            win = 'o' 
             return True
         if((self.gameboard[0][2] == 'o' and self.gameboard[1][2] == 'o' and self.gameboard[2][2]== 'o')):
             print("\no wins!")
+            win = 'o'
             return True
         
         # Diagonal wins O
         if((self.gameboard[0][0] == 'o' and self.gameboard[1][1] == 'o' and self.gameboard[2][2]== 'o')):
-            print("\no wins!") 
+            print("\no wins!")
+            win = 'o' 
             return True
         if((self.gameboard[0][2] == 'o' and self.gameboard[1][1] == 'x' and self.gameboard[2][0]== 'o')):
-            print("\no wins!") 
-            return True     
+            print("\no wins!")
+            win = 'o' 
+            return True   
         else: 
             return False
 
@@ -110,6 +128,16 @@ class Tictactoe:
             print(self.gameboard[2][0] + "|" + self.gameboard[2][1] + "|" + self.gameboard[2][2])
     def getGameBoard(self):
        return self.gameboard
+    
+
+    def outcome(self):
+       global win
+       if (win == 'o'):
+           return 1
+       elif (win == 'x'):
+           return -1
+       else:
+           return 0
 
 
         
